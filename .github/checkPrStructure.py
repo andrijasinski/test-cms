df9993c6f50dd6d213ed27e4bcd00698ff80dcea
Andri Jasinski <andri.jasinski@gmail.com>
2023-03-07 14:51:19 +0200

    Update checkPrStructure.py

--- .github/checkPrStructure.py
+++ .github/checkPrStructure.py
@@ -8,13 +8,23 @@ def main():
   print(f"Changed files are: {changed_files}")
   
   paths = list(map(lambda x: Path(x), changed_files))
-  metadata_path = list(filter(lambda x: x.name == METADATA_FILENAME, paths))
+  metadata_paths = list(filter(lambda x: x.name == METADATA_FILENAME, paths))
   
   print(f"Paths: {paths}")
-  print(f"Metadata paths: {metadata_path}")
+  print(f"Metadata paths: {metadata_paths}")
   
-  if len(metadata_path) == 0:
-    raise Exception(f"No {METADATA_FILENAME} is found, exiting")
+  if len(metadata_paths) == 0:
+    raise Exception(f"The {METADATA_FILENAME} is not found, exiting. The branch should have at least one {METADATA_FILENAME} file in the content root folder.")
+    
+  metadata_path = none
+  if len(metadata_paths) > 1:
+    metadata_path = min(list(map(lambda x: x.parent, metadata_paths)))
+  else:
+    metadata_path = metadata_paths[0]
+    
+  print(f"Files in that dir: {list(metadata_path.iterdir())}")
+    
+  
   
 
 if __name__ == "__main__":
